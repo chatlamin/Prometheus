f3253dfbdb00fecc574d23a3e8fe983a60a636d3
chatlamin <viktorap@inbox.ru>
2019-04-11 12:15:04 +0300

    alets

--- Prometheus/run/prometheus.sh
+++ Prometheus/run/prometheus.sh
@@ -3,6 +3,7 @@
 
 sudo mkdir -p /home/docker/containers/prometheus/{config,data}
 sudo touch /home/docker/containers/prometheus/config/prometheus.yml
+sudo touch /home/docker/containers/prometheus/config/alert.yml
 
 #что бы решить проблему с "opening storage failed: lock DB directory: open /prometheus/lock: permission denied" можно сделать так:
 #sudo chown 65534:65534 /home/docker/containers/prometheus/data
@@ -15,6 +16,7 @@ docker run --name prometheus \
     --restart=always \
     --user $ID \
     --volume /home/docker/containers/prometheus/config/prometheus.yml:/etc/prometheus/prometheus.yml \
+    --volume /home/docker/containers/prometheus/config/alert.yml:/etc/prometheus/alert.yml \
     --volume /home/docker/containers/prometheus/data:/prometheus \
     --publish 9090:9090  \
     prom/prometheus
